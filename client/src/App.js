831aa0847e7d51fb64d9fcaede1941f4f65b9ddb
emanherawy <eman.herawy@gmail.com>
2020-04-17 10:43:16 +0200

    set up the env to use the service

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,35 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
+import React, {
+  Component
+} from "react";
 
 import "./App.css";
+// import your service file here 
+import SimpleStorageService from './service/SimpleStorage'
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  constructor() {
+    super();
+    this.state = {
+      storageValue: 0,
+       account: "0xe092b1fa25DF5786D151246E492Eed3d15EA4dAA",
+       connected:false
+    };
 
+  }
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+      const service = new SimpleStorageService();
+      // Get the value from the contract to prove it worked.
+      const response = 0//await service.get(); //
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      // Update state with the result.
+      this.setState({
+        storageValue: response,
+        connected:true
+      });
+
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -34,22 +38,21 @@ class App extends Component {
       console.error(error);
     }
   };
-
   runExample = async () => {
-    const { accounts, contract } = this.state;
-
+    // Get the contract instance.
+    const service = new SimpleStorageService();
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
+    //await service.set(5, this.state.account, 200000);
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    const response = 0;//await service.get();
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({
+      storageValue: response
+    });
   };
-
   render() {
-    if (!this.state.web3) {
+    if (!this.state.connected) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
@@ -61,9 +64,9 @@ class App extends Component {
           If your contracts compiled and migrated successfully, below will show
           a stored value of 5 (by default).
         </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
+      <dive >
+      <button onClick = {this.runExample} > Add storage </button>
+      </dive>
         <div>The stored value is: {this.state.storageValue}</div>
       </div>
     );
